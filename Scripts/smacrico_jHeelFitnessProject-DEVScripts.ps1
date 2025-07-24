b5483a1d71e1d2a8f1e22405fdbb44c8125fc26c
stelios <smacricostas@gmail.com>
2025-07-24 16:47:49 +0300

    scripts added

---
+++ Scripts/smacrico_jHeelFitnessProject-DEVScripts.ps1
@@ -0,0 +1,131 @@
+﻿
+##################################
+#### jHeelFitness project#########
+######SCripts DEVelopment#########
+##################################
+
+JUly 7th, 2025,
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/GarminParse_PlugIn/jHeel_plugin v5.1 dev.py"
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_datawarehouse.py"
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_datawarehouse_V1.5.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_datawarehouse V1.9.py"
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV Patern Detection v1.0.py"
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_datawarehouse V1.95.py"
+
+
+
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/DayBlock.py"
+
+
+
+
+
+
+
+################################################
+################################################
+################################################
+
+### DEV Scripts **********************************
+#1:
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/GarminParse_PlugIn/jHeel_plugin v5.1 dev.py"
+#2 & #3:
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Apex_RunAnalyzer/runAnalysis/createRunAnalDB_dev.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Apex_RunAnalyzer/runAnalysis/RunningAnalysis_v50_DEV.py"
+
+
+########################
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/jHeel_plugin_v5.0fbbHRV_DEV.py"
+# consilidated in DWH HRV
+
+## Only MEditation fiels
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/HRVAnalyzer_v1.0_MED_DEV.py"
+# consilidated in DWH HRV
+
+### Modification Needed
+#######################
+###########
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/HRVdataAnalysis_modV1_v2.0_HRV_DEV.py"
+#############
+
+**********************************
+**********************************
+
+##################################
+#### myHealthData ################
+######MSBuddy Scripts    #########
+##################################
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/myHealthStats/basic_stats_v1.0.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/myHealthStats/basic_stats_v1.5.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/myHealthStats/myRuns_v1.0.py"
+
+##### Concolidated fbb and MED scripts to create HRV_DataWareHouse ###################
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_datawarehouse.py"
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_DataWareHouse_v1.5.py"
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/HRV_DataWareHouse v1.5.py"
+
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/HRV_DataWareHouse_v1.6.py"
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/DayBlock.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRV_DWH/DayBlockAverages.py"
+
+
+
+
+
+##### HRV Main - enhanced_hrv_analysis -
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRVmain_v1.5.py"
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/HRVmain_v1.0.py"
+
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/MSBuddy_FitnessApp/jHeel_v1.0.py"
+
+
+
+
+
+C:\smakryko\myHealthData\CG_jHeelFitnessProject\Mercury_HRV\HRVAnalyzer
+
+
+
+
+& C:/Users/XP222SP/AppData/Local/Programs/Python/Python312/python.exe "C:/smakryko/myHealthData/CG_jHeelFitnessProject/
+
+
+**************************
+**************************
+## Actual Scripts to Run for Production / AFTER Garmin Purse - dataBases ####
+#1
+& C:/Python313/python.exe "C:/smakrykoDev/Phoenix/CG_jHeelFitnessProject/GarminParse_PlugIn/jHeel_plugin v5.0.py"
+
+
+#2
+& C:/Python313/python.exe "C:/smakrykoDev/Phoenix/CG_jHeelFitnessProject/Apex_RunAnalyzer/runAnalysis/createRunAnalDB.py"
+
+#3 RunAnalysis ###
+& C://Python313/python.exe "C:/smakrykoDev/Phoenix/CG_jHeelFitnessProject/Apex_RunAnalyzer/runAnalysis/RunningAnalysis_v50.py"
+
+#4
+& C:/Users/djsco/AppData/Local/Programs/Python/Python312/python.exe "C:/SteliosDev/CosmosGenesisLocal/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/jHeel_plugin_v5.0fbbHRV.py"
+
+
+#5 HRV Analyzer
+& C:/Users/djsco/AppData/Local/Programs/Python/Python312/python.exe "C:/SteliosDev/CosmosGenesisLocal/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/HRVAnalyzer_v1.0_MED.py"
+& C:/Users/djsco/AppData/Local/Programs/Python/Python312/python.exe "C:/SteliosDev/CosmosGenesisLocal/CG_jHeelFitnessProject/Mercury_HRV/HRVAnalyzer/HRVdataAnalysis_modV1_v2.0_HRV.py"
